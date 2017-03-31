d5a1587db1f99383cdf195ea08029eae5d4d2fc3
sfhsdfasg <gertpaimla@gmail.com>
2017-03-31 12:18:57 +0300

    Update Ore.cs

--- Ore.cs
+++ Ore.cs
@@ -1,26 +1,16 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-using Android.App;
-using Android.Content;
-using Android.OS;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
-
 namespace minefine
 {
     public class Ore
     {
-        public string name { get; set; }
-        public string image { get; set; }
-
-        public Ore(string name, string image)
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public int OreCount { get; set; }
+        public Ore(string name, string image, int orecount)
         {
-            this.name = name;
-            this.image = name;
+            this.Name = name;
+            this.Image = name;
+            this.OreCount = orecount;
         }
     }
 }
+
