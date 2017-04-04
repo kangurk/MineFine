6dd692675ac587f9cac8845eaab4e80ed9a20308
sfhsdfasg <gertpaimla@gmail.com>
2017-04-04 15:33:48 +0300

    Update OreAdapter.cs

--- OreAdapter.cs
+++ OreAdapter.cs
@@ -8,13 +8,21 @@ using Android.Views;
 using Android.Widget;
 using System.Collections.ObjectModel;
 
-namespace minefine
+namespace MineFine
 {
     public class OreAdapter : BaseAdapter<Ore>
     {
         ObservableCollection<Ore> ores;
         Activity context;
-
+        Dictionary<string, int> imageResources = new Dictionary<string, int>() {
+            { "Copper_Ore",Resource.Drawable.Copper_Ore},
+            {"Tin_Ore",Resource.Drawable.Tin_Ore},
+            {"Iron_Ore",Resource.Drawable.Iron_Ore},
+            {"Silver_Ore",Resource.Drawable.Silver_Ore},
+            {"Mithril_Ore",Resource.Drawable.Mithril_Ore},
+            {"Adamantite_Ore",Resource.Drawable.Adamantite_Ore},
+            {"Runite_Ore",Resource.Drawable.Runite_Ore},
+            {"Coal_Ore",Resource.Drawable.Coal_Ore}};
         public OreAdapter(Activity context, ObservableCollection<Ore> ores) : base()
         {
             this.context = context;
@@ -41,13 +49,15 @@ namespace minefine
             View view = convertView;
             if (view == null)
             {
-                view = context.LayoutInflater.Inflate(Android.Resource.Layout.ActivityListItem, null);
+                view = context.LayoutInflater.Inflate(Resource.Layout.ShopListViewLayout, null);
             }
 
             var oreObject = ores[position];
 
-            view.FindViewById<TextView>(Android.Resource.Id.Text1).Text = oreObject.Name;
-            ImageView img = view.FindViewById<ImageView>(Android.Resource.Id.Icon);
+            view.FindViewById<TextView>(Resource.Id.amountOfOre).Text = oreObject.OreCount.ToString();
+            view.FindViewById<TextView>(Resource.Id.oreName).Text = oreObject.Name;
+            view.FindViewById<ImageView>(Resource.Id.listViewOreImage).SetImageResource(imageResources[oreObject.Name]);
+            
 
             return view;
         }
