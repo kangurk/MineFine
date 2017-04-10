faef995db3d40a5b83d5c295188e0431171ed06d
OttKirss <Ott.Kirss@gmail.com>
2017-04-10 09:41:11 +0300

    Update ShopActivity.cs

--- ShopActivity.cs
+++ ShopActivity.cs
@@ -1,21 +1,40 @@
 using Android.App;
 using Android.OS;
 using Android.Widget;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
-namespace MineFine
+namespace minefine
 {
     [Activity(Label = "ShopActivity")]
     public class ShopActivity : Activity
     {
         DatabaseDataHandler databaseDataHandler = DatabaseDataHandler.Instance;
+        string currentOre = "Copper_Ore";
+        ObservableCollection<Ore> oreList;
+        TextView Currency;
+        int gp = 0;
+        Dictionary<string, int> oreExp = new Dictionary<string, int>()
+        {
+            {"Copper_Ore", 10},
+            {"Tin_Ore", 17},
+            {"Iron_Ore", 30},
+            {"Silver_Ore", 40},
+            {"Coal_Ore", 50},
+            {"Mithril_Ore", 80},
+            {"Adamantite_Ore", 95},
+            {"Runite_Ore",125 },
+        };
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
-            SetContentView(Resource.Layout.Shop);
-            var text = FindViewById<TextView>(Resource.Id.Ore);
+            oreList = databaseDataHandler.getObservable();
 
-            var oreAdapter = new OreAdapter(this, databaseDataHandler.getObservable());
+            SetContentView(Resource.Layout.Shop);
+            Currency = FindViewById<TextView>(Resource.Id.gp);
+
+            var oreAdapter = new OreAdapter(this, oreList);
             var shopListView = FindViewById<ListView>(Resource.Id.shopMenu);
             shopListView.Adapter = oreAdapter;
             shopListView.ItemClick += ShopListView_ItemClick;
@@ -23,8 +42,25 @@ namespace MineFine
 
         private void ShopListView_ItemClick(object sender, AdapterView.ItemClickEventArgs e)
         {
-            Toast.MakeText(this, databaseDataHandler.getObservable()[e.Position].Name, ToastLength.Short).Show();
+            Toast.MakeText(this, oreList[e.Position].Name, ToastLength.Short).Show();
             //todo messagebox
+            AlertDialog.Builder alert = new AlertDialog.Builder(this);
+            alert.SetMessage("Do you want to sell or choose this ore to click?");
+            alert.SetPositiveButton("Sell ore", (senderAlert, args) => {
+                gp = gp + (oreExp[currentOre] * oreList[e.Position].OreCount);
+                Currency.Text = gp.ToString() + "gp";
+                int oreCount = 0;
+                
+            });
+            alert.SetNegativeButton("Cancel", (senderAlert, args) => {
+                Dialog dialogg = alert.Create();
+                dialogg.Hide();
+            });
+            alert.SetNeutralButton("Choose ore", (senderAlert, args) => {
+                currentOre = oreList[e.Position].Name;
+            });
+            Dialog dialog = alert.Create();
+            dialog.Show();
         }
     }
 }
