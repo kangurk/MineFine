ad047dae95942f3565223cffa62a7063e0b84874
sfhsdfasg <gertpaimla@gmail.com>
2017-04-13 18:26:45 +0300

    Update ShopActivity.cs

--- ShopActivity.cs
+++ ShopActivity.cs
@@ -1,30 +1,21 @@
 using Android.App;
+using Android.Content;
 using Android.OS;
 using Android.Widget;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 
-namespace minefine
+namespace MineFine
 {
     [Activity(Label = "ShopActivity")]
     public class ShopActivity : Activity
     {
         DatabaseDataHandler databaseDataHandler = DatabaseDataHandler.Instance;
-        string currentOre = "Copper_Ore";
         ObservableCollection<Ore> oreList;
         TextView Currency;
-        int gp = 0;
-        Dictionary<string, int> oreExp = new Dictionary<string, int>()
-        {
-            {"Copper_Ore", 10},
-            {"Tin_Ore", 17},
-            {"Iron_Ore", 30},
-            {"Silver_Ore", 40},
-            {"Coal_Ore", 50},
-            {"Mithril_Ore", 80},
-            {"Adamantite_Ore", 95},
-            {"Runite_Ore",125 },
-        };
+        ListView shopListView;
+        OreAdapter oreAdapter;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -33,34 +24,53 @@ namespace minefine
 
             SetContentView(Resource.Layout.Shop);
             Currency = FindViewById<TextView>(Resource.Id.gp);
+            Currency.Text = databaseDataHandler.Currency.ToString();
 
-            var oreAdapter = new OreAdapter(this, oreList);
-            var shopListView = FindViewById<ListView>(Resource.Id.shopMenu);
+            oreAdapter = new OreAdapter(this, oreList);
+           
+            shopListView = FindViewById<ListView>(Resource.Id.shopMenu);
             shopListView.Adapter = oreAdapter;
             shopListView.ItemClick += ShopListView_ItemClick;
+
+            
         }
 
         private void ShopListView_ItemClick(object sender, AdapterView.ItemClickEventArgs e)
         {
             Toast.MakeText(this, oreList[e.Position].Name, ToastLength.Short).Show();
-            //todo messagebox
-            AlertDialog.Builder alert = new AlertDialog.Builder(this);
-            alert.SetMessage("Do you want to sell or choose this ore to click?");
-            alert.SetPositiveButton("Sell ore", (senderAlert, args) => {
-                gp = gp + (oreExp[currentOre] * oreList[e.Position].OreCount);
-                Currency.Text = gp.ToString() + "gp";
-                int oreCount = 0;
-                
-            });
-            alert.SetNegativeButton("Cancel", (senderAlert, args) => {
-                Dialog dialogg = alert.Create();
-                dialogg.Hide();
-            });
-            alert.SetNeutralButton("Choose ore", (senderAlert, args) => {
-                currentOre = oreList[e.Position].Name;
-            });
-            Dialog dialog = alert.Create();
-            dialog.Show();
+            Bundle args = new Bundle();
+            args.PutString("oreName", oreList[e.Position].Name);
+            args.PutString("oreCount", oreList[e.Position].OreCount.ToString());
+            createFragment(args,e.Position);
+            
         }
+        
+
+        private void createFragment(Bundle args, int pos)
+        {
+            FragmentTransaction ft = FragmentManager.BeginTransaction();
+            //Remove fragment else it will crash as it is already added to backstack
+            Fragment prev = FragmentManager.FindFragmentByTag("shopDialog");
+            if (prev != null)
+            {
+                ft.Remove(prev);
+            }
+            ft.AddToBackStack(null);
+            // Create and show the dialog.
+            DialogFragmentCustom newFragment = DialogFragmentCustom.NewInstance(args);
+            newFragment.Dismissed += (s, e) => { Currency.Text = e.Text; oreList[pos].OreCount = 0; newFragment.Dismiss(); oreAdapter.NotifyDataSetChanged(); };
+            //Add fragment
+            newFragment.Show(ft, "shopDialog");
+            
+        }
+
+
     }
+    public class DialogEventArgs : EventArgs
+    {
+        public string Text { get; set; }
+
+    }
+
+    public delegate void DialogEventHandler(object sender, DialogEventArgs args);
 }
