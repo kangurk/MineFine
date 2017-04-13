d5a23a74fca699e49eb97f16b4d0f1787d7aeddc
sfhsdfasg <gertpaimla@gmail.com>
2017-04-13 18:26:59 +0300

    Update OreAdapter.cs

--- OreAdapter.cs
+++ OreAdapter.cs
@@ -14,15 +14,6 @@ namespace MineFine
     {
         ObservableCollection<Ore> ores;
         Activity context;
-        Dictionary<string, int> imageResources = new Dictionary<string, int>() {
-            { "Copper_Ore",Resource.Drawable.Copper_Ore},
-            {"Tin_Ore",Resource.Drawable.Tin_Ore},
-            {"Iron_Ore",Resource.Drawable.Iron_Ore},
-            {"Silver_Ore",Resource.Drawable.Silver_Ore},
-            {"Mithril_Ore",Resource.Drawable.Mithril_Ore},
-            {"Adamantite_Ore",Resource.Drawable.Adamantite_Ore},
-            {"Runite_Ore",Resource.Drawable.Runite_Ore},
-            {"Coal_Ore",Resource.Drawable.Coal_Ore}};
         public OreAdapter(Activity context, ObservableCollection<Ore> ores) : base()
         {
             this.context = context;
@@ -56,7 +47,7 @@ namespace MineFine
 
             view.FindViewById<TextView>(Resource.Id.amountOfOre).Text = oreObject.OreCount.ToString();
             view.FindViewById<TextView>(Resource.Id.oreName).Text = oreObject.Name;
-            view.FindViewById<ImageView>(Resource.Id.listViewOreImage).SetImageResource(imageResources[oreObject.Name]);
+            view.FindViewById<ImageView>(Resource.Id.listViewOreImage).SetImageResource(oreObject.Image);
             
 
             return view;
