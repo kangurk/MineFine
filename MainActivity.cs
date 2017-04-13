9c1c24f9f556d9ae085016351c7bd0f8b51af8d4
sfhsdfasg <gertpaimla@gmail.com>
2017-04-13 18:28:09 +0300

    Update MainActivity.cs

--- MainActivity.cs
+++ MainActivity.cs
@@ -12,45 +12,38 @@ namespace MineFine
     [Activity(Label = "minefine", MainLauncher = true, Icon = "@drawable/icon", ScreenOrientation = ScreenOrientation.Portrait)]
     public class MainActivity : Activity
     {
-        string currentOre = "Copper_Ore";
+        
         int experience = 0;
         int expLevel = 1;
         double nextLevel = 83;
         int cooldownSeconds = 5;
-        string positive;
-        string negative;
-        string question;
 
         //class on padlockitud, et ainult 1 instance saab sellest olla
         DatabaseDataHandler databaseDataHandler = DatabaseDataHandler.Instance;
-
-
-        Dictionary<string, int> oreExp = new Dictionary<string, int>()
-        {
-            {"Copper_Ore", 10},
-            {"Tin_Ore", 17},
-            {"Iron_Ore", 30},
-            {"Silver_Ore", 40},
-            {"Coal_Ore", 50},
-            {"Mithril_Ore", 80},
-            {"Adamantite_Ore", 95},
-            {"Runite_Ore",125 },
-        };
+        Ore currentActiveOre;
         ObservableCollection<Ore> oreCount;
+        Button toShop;
+        ImageView mainImage;
+        TextView expHour;
+        TextView totalExp;
+        TextView totalLevel;
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
+
             SetContentView(Resource.Layout.Main);
-            var toShop = FindViewById<Button>(Resource.Id.toShop);
-            var mainImage = FindViewById<ImageView>(Resource.Id.mainImage);
-            var expHour = FindViewById<TextView>(Resource.Id.expHour);
-            var totalExp = FindViewById<TextView>(Resource.Id.totalExp);
-            var totalLevel = FindViewById<TextView>(Resource.Id.Level);
+            toShop = FindViewById<Button>(Resource.Id.toShop);
+            mainImage = FindViewById<ImageView>(Resource.Id.mainImage);
+            expHour = FindViewById<TextView>(Resource.Id.expHour);
+            totalExp = FindViewById<TextView>(Resource.Id.totalExp);
+            totalLevel = FindViewById<TextView>(Resource.Id.Level);
 
             initializeDatabase();
+
             totalExp.Text = "total experience is " + experience.ToString();
             totalLevel.Text = "Your current level is " + expLevel.ToString();
 
+            
             toShop.Click += delegate
             {
                 StartActivity(typeof(ShopActivity));
@@ -60,9 +53,8 @@ namespace MineFine
             {
                 //translation for this line = selecti observablelistist Ore class mille Ore.Name on sama nimega mis currentOre ja liida sellele 1 
                 //ja kuna observablecollection on reference databasedatahandler classi variablist siis muutub see value ka databasedatahandler classi observablecollectionis
-                oreCount.Select((p, i) => new { Ore = p, Index = i }).Where(p => p.Ore.Name == currentOre).First().Ore.OreCount += 1;
-
-                experience += oreExp[currentOre];
+                currentActiveOre.OreCount += 1;
+                experience += currentActiveOre.OreExpRate;
                 totalExp.Text = "total experience is " + experience.ToString();
                 Level();
                 totalLevel.Text = "Your current level is " + expLevel.ToString();
@@ -82,55 +74,13 @@ namespace MineFine
         }
         private void randomEvents()
         {
-            List<String> events = new List<String>();
-            events.Add("Genie");
-            events.Add("Surprise exam");
-            events.Add("Frog");
-            events.Add("Quiz");
-            Random npcChance = new Random();
-            var chance1 = npcChance.Next(1, 4);
             Java.Util.Random eventChance = new Java.Util.Random();
-            var chance = eventChance.NextInt(10);
+            var chance = eventChance.NextInt(100);
             if (chance == 1)
             {
-                AlertDialog.Builder alert = new AlertDialog.Builder(this);
-                alert.SetTitle("RandomEvent!!");
-                switch (events[chance1 - 1])
-                {
-                    case "Genie":
-                        alert.SetIcon(Resource.Drawable.genie);
-                        positive = "Rub the lamp!";
-                        negative = "Dismiss genie!";
-                        question = "A genie has appeared, rub the lamp for a fortune";
-                        break;
-                    case "Surprise exam":
-                        alert.SetIcon(Resource.Drawable.exam);
-                        positive = "Mine Fine";
-                        negative = "Ore Clicker";
-                        question = "Which of the is the correct pronunciation";
-                        break;
-                    case "Frog":
-                        alert.SetIcon(Resource.Drawable.frog);
-                        positive = "Of course!";
-                        negative = "Ew, no!";
-                        question = "Will you kiss the frog?";
-                        break;
-                    case "Quiz":
-                        alert.SetIcon(Resource.Drawable.quiz);
-                        positive = "No";
-                        negative = "Yes";
-                        question = "Can you land on sun during the night?";
-                        break;
-                }
-                alert.SetMessage(question);
-                alert.SetPositiveButton(positive, (senderAlert, args) => {
-                    //auhind kui õigesti vastad
-                });
-                alert.SetNegativeButton(negative, (senderAlert, args) => {
-                    //auhind kui valesti vastad
-                });
-                Dialog dialog = alert.Create();
-                dialog.Show();
+                var alert = new AlertDialog.Builder(this);
+                alert.SetView(LayoutInflater.Inflate(Resource.Layout.RandomEvent, null));
+                alert.Create().Show();
             }
         }
 
@@ -159,6 +109,7 @@ namespace MineFine
             experience = values.Item1;
             expLevel = values.Item2;
             oreCount = databaseDataHandler.getObservable();
+
         }
         protected override void OnPause()
         {
@@ -169,6 +120,12 @@ namespace MineFine
         {
             base.OnDestroy();
             databaseDataHandler.saveDataDatabase(experience, expLevel); // ei tea millal(/)kuhu seda panna veel
+        }
+        protected override void OnResume()
+        {
+            base.OnResume();
+            currentActiveOre = oreCount.Select((p) => new { Ore = p }).Where(p => p.Ore.Name == databaseDataHandler.CurrentOre).First().Ore;
+            mainImage.SetImageResource(currentActiveOre.Image);
         }
 
     }
