c5a011d633fd81ee5aa069578e79e59f9e0eee1a
sfhsdfasg <gertpaimla@gmail.com>
2017-04-17 17:11:46 +0300

    Update MainActivity.cs

--- MainActivity.cs
+++ MainActivity.cs
@@ -6,15 +6,15 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using Android.Content.PM;
 using System;
+using Android.Views;
 
 namespace MineFine
 {
     [Activity(Label = "minefine", MainLauncher = true, Icon = "@drawable/icon", ScreenOrientation = ScreenOrientation.Portrait)]
     public class MainActivity : Activity
     {
-        
-        int experience = 0;
-        int expLevel = 1;
+
+
         double nextLevel = 83;
         string question;
         string positive;
@@ -23,8 +23,7 @@ namespace MineFine
         //class on padlockitud, et ainult 1 instance saab sellest olla
         DatabaseDataHandler databaseDataHandler = DatabaseDataHandler.Instance;
         Ore currentActiveOre;
-        ObservableCollection<Ore> oreCount;
-        
+
         Button toShop;
         ImageView mainImage;
         TextView expHour;
@@ -39,15 +38,16 @@ namespace MineFine
             SetContentView(Resource.Layout.Main);
             toShop = FindViewById<Button>(Resource.Id.toShop);
             mainImage = FindViewById<ImageView>(Resource.Id.mainImage);
+            expHour = FindViewById<TextView>(Resource.Id.expHour);
             totalExp = FindViewById<TextView>(Resource.Id.totalExp);
             totalLevel = FindViewById<TextView>(Resource.Id.Level);
 
             initializeDatabase();
 
-            totalExp.Text = "total experience is " + experience.ToString();
-            totalLevel.Text = "Your current level is " + expLevel.ToString();
+            totalExp.Text = "total experience is " + databaseDataHandler.UserData.Experience.ToString();
+            totalLevel.Text = "Your current level is " + databaseDataHandler.UserData.Level.ToString();
 
-            
+
             toShop.Click += delegate
             {
                 StartActivity(typeof(ShopActivity));
@@ -58,22 +58,21 @@ namespace MineFine
                 //translation for this line = selecti observablelistist Ore class mille Ore.Name on sama nimega mis currentOre ja liida sellele 1 
                 //ja kuna observablecollection on reference databasedatahandler classi variablist siis muutub see value ka databasedatahandler classi observablecollectionis
                 currentActiveOre.OreCount += 1;
-                experience += currentActiveOre.OreExpRate;
-                totalExp.Text = "total experience is " + experience.ToString();
+                databaseDataHandler.UserData.Experience += currentActiveOre.OreExpRate;
+                totalExp.Text = "total experience is " + databaseDataHandler.UserData.Experience.ToString();
                 Level();
-                totalLevel.Text = "Your current level is " + expLevel.ToString();
+                totalLevel.Text = "Your current level is " + databaseDataHandler.UserData.Level.ToString();
                 randomEvents();
             };
         }
         private int Level()
         {
-            if (experience > nextLevel)
+            if (databaseDataHandler.UserData.Experience > nextLevel)
             {
                 double temp = nextLevel * 1.1;
                 double together = nextLevel + temp;
-                expLevel += 1;
-                nextLevel = Convert.ToInt32(together);
-                levelUp();
+                databaseDataHandler.UserData.Level += 1;
+                nextLevel = together;
             }
             return 0;
         }
@@ -135,6 +134,8 @@ namespace MineFine
 
         private void levelUp()
         {
+            //var levelupText = FindViewById<TextView>(Resource.Id.levelupText);
+            //levelupText.Text = "You've achieved level " + expLevel.ToString();
             var alert = new AlertDialog.Builder(this);
             alert.SetView(LayoutInflater.Inflate(Resource.Layout.levelUp, null));
             alert.Create().Show();
@@ -152,26 +153,24 @@ namespace MineFine
         /// </summary>
         private void initializeDatabase()
         {
-            Tuple<int,int,int> values = databaseDataHandler.getDataFromDatabase();
-            experience = values.Item1;
-            expLevel = values.Item2;
-            oreCount = databaseDataHandler.getObservable();
-            nextLevel = values.Item3;
+            databaseDataHandler.initializeData();
+
+
         }
         protected override void OnPause()
         {
             base.OnPause();
-            databaseDataHandler.saveDataDatabase(experience, expLevel, nextLevel); // ei tea millal(/)kuhu seda panna veel
+            databaseDataHandler.saveDataDatabase(); // ei tea millal(/)kuhu seda panna veel
         }
         protected override void OnDestroy()
         {
             base.OnDestroy();
-            databaseDataHandler.saveDataDatabase(experience, expLevel, nextLevel);// ei tea millal(/)kuhu seda panna veel
+            databaseDataHandler.saveDataDatabase(); // ei tea millal(/)kuhu seda panna veel
         }
         protected override void OnResume()
         {
             base.OnResume();
-            currentActiveOre = databaseDataHandler.getObservable().Select((p) => new { Ore = p }).Where(p => p.Ore.Name == databaseDataHandler.CurrentOre).First().Ore;
+            currentActiveOre = databaseDataHandler.OreObservableList.Select((p) => new { Ore = p }).Where(p => p.Ore.Name == databaseDataHandler.UserData.CurrentOre).First().Ore;
             mainImage.SetImageResource(currentActiveOre.Image);
         }
 
