bfa893e6deb4ebc9ca3cbb28126163e65ff3fe24
sfhsdfasg <gertpaimla@gmail.com>
2017-04-17 17:10:47 +0300

    Update Pickaxe.cs

--- Pickaxe.cs
+++ Pickaxe.cs
@@ -1,1 +1,19 @@
+namespace MineFine
+{
+    public class Pickaxe
+    {
+        public string Name { get; set; }
+        public int CoolDownRate { get; set; }
+        public int Cost { get; set; }
+
+        public Pickaxe(string name, int cdrate, int cost)
+        {
+            this.Name = name;
+            this.CoolDownRate = cdrate;
+            this.Cost = cost;
+
+        }
+    }
+}
+
 
