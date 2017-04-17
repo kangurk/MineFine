0d37b7daf8eae7a04275eb3dfe7fadf38f1beaff
sfhsdfasg <gertpaimla@gmail.com>
2017-04-17 17:11:32 +0300

    Update Ore.cs

--- Ore.cs
+++ Ore.cs
@@ -6,15 +6,16 @@ namespace MineFine
         public int Image { get; set; }
         public int OreCount { get; set; }
         public int OreCurrencyValue { get; set; }
-
         public int OreExpRate { get; set; }
-        public Ore(string name, int image, int orecount, int orecurrencyvalue, int oreexprate)
+        public bool IsOreUnlockedByUser { get; set; }
+        public Ore(string name, int image, int orecount, int orecurrencyvalue, int oreexprate, bool IsOreUnlockedByUser)
         {
             this.Name = name;
             this.Image = image;
             this.OreCount = orecount;
             this.OreCurrencyValue = orecurrencyvalue;
             this.OreExpRate = oreexprate;
+            this.IsOreUnlockedByUser = IsOreUnlockedByUser;
         }
     }
 }
