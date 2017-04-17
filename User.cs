6b2ac0df7b10519501df9dafdfb7d272e21b5c13
sfhsdfasg <gertpaimla@gmail.com>
2017-04-17 17:10:21 +0300

    Update User.cs

--- User.cs
+++ User.cs
@@ -1,1 +1,16 @@
+namespace MineFine
+{
+    public class User
+    {
+        public Pickaxe UserPickaxe { get; set; }
+        public int Currency { get; set; }
+        public int Experience { get; set; }
+        public int Level { get; set; }
+        public int CurrentPickaxeIndex { get; set; }
+        public string CurrentOre { get; set; }
+        public User()
+        {
 
+        }
+    }
+}
